39300fe267e6d74bf526d2bbd911c055cc692b1a
martinsLaptop <augustuswww@email.de>
2021-08-23 22:54:35 +0200

    alles

---
+++ app/src/main/java/com/jakobzeise/documentreader/ReadingActivity.kt
@@ -0,0 +1,11 @@
+package com.jakobzeise.documentreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reading)
+    }
+}
