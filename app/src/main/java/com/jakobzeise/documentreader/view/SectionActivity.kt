cbcd3d7e9a16245f65fd4f653c21b808babe766f
BuildTools <unconfigured@null.spigotmc.org>
2021-09-15 21:06:29 +0200

    projectList is null when returning from the readingActivity

--- app/src/main/java/com/jakobzeise/documentreader/view/SectionActivity.kt
+++ app/src/main/java/com/jakobzeise/documentreader/view/SectionActivity.kt
@@ -9,11 +9,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.jakobzeise.documentreader.R
+import com.jakobzeise.documentreader.modell.ReadList
 import kotlinx.android.synthetic.main.activity_section.*
 
 var projectNumber = -1
 var fileName = ""
 var fileContent = ""
+var readListProjects = mutableListOf<ReadList>()
 var readList = mutableListOf<Boolean>()
 var falseList = mutableListOf<Boolean>()
 
@@ -24,30 +26,68 @@ class SectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_section)
 
+        //shared preferences for saving the data
         sharedPreferences = getSharedPreferences("sectionReader", Context.MODE_PRIVATE)
+
+        //initialises the editor
         val editor: SharedPreferences.Editor? = sharedPreferences?.edit()
 
-
+        //current project number from the main Activity intent or the Readingactivity Intent
         projectNumber = intent.getIntExtra("projectNumber", -1)
+
+        //fileName and fileContent from the public projectList
         fileName = listOfProjects[projectNumber].fileName
         fileContent = listOfProjects[projectNumber].fileContent
 
-        val sectionList = fileContent.let { fileReader.getSectionsFromString(it) }
-        val readListFromReadingActivity = intent.getBooleanArrayExtra("readList")
-        readListFromReadingActivity?.let {
-            val json = Gson().toJson(readListFromReadingActivity)
-            if (editor != null) {
-                editor.putString("readListKey", json)
-                editor.apply()
-            }
+        //separates the fileContent of the current Project into multiple sections
+        val sectionList = fileContent.let {
+            fileReader.getSectionsFromString(it)
         }
 
+        //gets the ReadList from the readingActivity
+        val readListFromReadingActivity = intent.getBooleanArrayExtra("readList")
+
+        //Deletes the current readList from the readListWithNumber list
+        readListProjects.filter {
+            //deletes the current entry
+            it.fileNumber != projectNumber
+        }
+
+        //Adds the readList from the readingActivtiy
+
+        if (readListFromReadingActivity == null) {
+            readListProjects.add(
+                ReadList(
+                    projectNumber,
+                    fileReader.getSectionsFromString(fileContent) as MutableList<Boolean>
+                )
+            )
+        } else {
+            readListProjects.add(
+                ReadList(
+                    projectNumber,
+                    readListFromReadingActivity as MutableList<Boolean>
+                )
+            )
+
+        }
+
+        //Converts the readListProjects list into a String and puts it into sharedPreferences
+        val json = Gson().toJson(readListProjects)
+        if (editor != null) {
+            editor.putString("readListKey", json)
+            editor.apply()
+        }
+
+
+        //if there already is a entry in sharedPreferences add this to readListProjects
         val temp = sharedPreferences?.getString("readListKey", null)
         if (!temp.isNullOrEmpty()) {
-            readList = Gson().fromJson<ArrayList<Boolean>>(temp)
+            readListProjects = Gson().fromJson<MutableList<ReadList>>(temp)
         }
 
-        if (readList.isNullOrEmpty()) {
+
+        if (readListProjects[projectNumber].fileNumber == projectNumber) {
 
             readList = if (intent.getBooleanArrayExtra("readList") == null) {
                 for ((_) in sectionList.withIndex()) {
@@ -55,22 +95,16 @@ class SectionActivity : AppCompatActivity() {
                 }
                 falseList
             } else {
-                intent.getBooleanArrayExtra("readList")!!.toMutableList()
+                readListProjects[projectNumber].readList
             }
         } else {
             falseList = readList
         }
 
-
-        val numberList = mutableListOf<Int>()
-
-        var i = 1
-        for (section in sectionList) {
-            numberList.add(i++)
-        }
-
+        //Sets the text of the textView to the fileName
         textViewProjectName.text = fileName
 
+        //Adjust the RecyclerView
         GridLayoutManager(
             this,
             3,
@@ -79,9 +113,17 @@ class SectionActivity : AppCompatActivity() {
         ).apply {
             recyclerViewSectionActivity.layoutManager = this
         }
+
+        val numberList = mutableListOf<Int>()
+
+        var i = 1
+        for (section in sectionList) {
+            numberList.add(i++)
+        }
         recyclerViewSectionActivity.adapter = RecyclerAdapterSectionActivity(numberList)
     }
 
+    //Inline Function for converting a collection into a json String
     inline fun <reified T> Gson.fromJson(json: String) =
         fromJson<T>(json, object : TypeToken<T>() {}.type)
 }
